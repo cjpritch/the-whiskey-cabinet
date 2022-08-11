dcf29f45e43382764a0ed5943f699146feb4a77b
Carly Pritchard <cpritch616@gmail.com>
2022-08-11 17:26:09 -0500

    create review model

--- models/Review.js
+++ models/Review.js
@@ -1,1 +1,48 @@
-// creates a new whiskey review
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+// creates new review 
+Review.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+    rating: {
+        type:DataTypes.INTEGER,
+        allowNull: false
+    },
+    review_text: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'post',
+          key: 'id'
+        }
+      }
+    },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review'
+  }
+);
+
+module.exports = Review;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Review = require("./Review");
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,4 +10,20 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+Review.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Review.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Review, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Review, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Review };
